e705beccc45a5209c1da6d8427085403d3cbad1b
Samay Jain <samayjain666@gmail.com>
2024-06-22 10:27:51 +0530

    added spinner component

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -21,12 +21,6 @@ const Spinner = () => {
       <div className="spinner-grow text-info" role="status">
         <span className="visually-hidden">Loading...</span>
       </div>
-      <div className="spinner-grow text-light" role="status">
-        <span className="visually-hidden">Loading...</span>
-      </div>
-      <div className="spinner-grow text-dark" role="status">
-        <span className="visually-hidden">Loading...</span>
-      </div>
     </div>
   );
 };
